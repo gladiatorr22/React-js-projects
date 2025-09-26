653b1e6d4fc63bf12b379ee836ab2446af64ea0a
gladiatorr22 <likhitnataraj@gmail.com>
2025-09-26 18:43:14 +0530

    custom-react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,8 +1,33 @@
-
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
+
+const anotherelement = (
+  <a href='https://google.com' target='_blank'>visit google</a>
+)
+
+const anotheruser = 'likith'
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com' ,target:'_blank'},
+  'click me for google',
+  anotheruser
+)
+
+function MyApp(){
+  const username = 'likith'
+  return(
+    <div>
+      <h1>username = {username}</h1> 
+    </div>
+    
+  )
+}
+
+
 createRoot(document.getElementById('root')).render(
-    <App />
+    reactElement
 )
