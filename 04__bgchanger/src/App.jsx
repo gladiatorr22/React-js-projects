42c48ae1b6d2fdf824b33ee4ba4a67c4e10080e5
gladiatorr22 <likhitnataraj@gmail.com>
2025-11-14 19:00:13 +0530

    feat: initialize bgchanger project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies for React, Vite, and Tailwind CSS.
    - Created main application structure including App.jsx, main.jsx, and styles.
    - Implemented background color changer functionality in App component.
    - Added SVG assets for React and Vite logos.
    - Configured Vite with React and Tailwind CSS plugins.

---
+++ 04__bgchanger/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl '>
+          <button onClick={()=>setColor('#690005ff')} className='outline-none px-4 py-1 rounded-full text-amber-100 shadow-lg'
+          style={{backgroundColor:'#690005ff'}}
+          >red</button>
+
+          <button onClick={()=>setColor('green')} className='outline-none px-4 py-1 rounded-full text-amber-100 shadow-lg'
+          style={{backgroundColor:'green'}}
+          >green</button>
+
+          <button onClick={()=>setColor('#0d3b72ff')} className='outline-none px-4 py-1 rounded-full text-amber-100 shadow-lg'
+          style={{backgroundColor:'#0d3b72ff'}}
+          >blue</button>
+        </div>
+      </div>  
+
+    </div>
+  );
+}
+
+export default App;
